59ce009cfe449cab5da42edf44e18ac0225879b9
Rohit Rohit <rohitsaroha125@gmail.com>
2024-04-03 09:54:46 +0530

    single meal detail page added

--- app/meals/[slug]/page.tsx
+++ app/meals/[slug]/page.tsx
@@ -1,3 +1,30 @@
-export default function MealsDetailPage({ params }: { params: any }) {
-  return <h1>Meals - {params.slug}</h1>;
+import styles from "./page.module.css";
+import Image from "next/image";
+import { getMeal } from "@/lib/meals";
+
+export default async function MealsDetailPage({ params }: { params: any }) {
+  const meal = await getMeal(params.slug);
+
+  return (
+    <>
+      <header className={styles.header}>
+        <div className={styles.image}>
+          <Image src={meal.image} fill alt={`${meal.title} - Image`} />
+        </div>
+        <div className={styles.headerText}>
+          <h1>{meal.title}</h1>
+          <p className={styles.creator}>
+            by <a href="mailto:email">{meal.creator}</a>
+          </p>
+          <p className={styles.summary}>{meal.summary}</p>
+        </div>
+      </header>
+      <main>
+        <p
+          className={styles.instructions}
+          dangerouslySetInnerHTML={{ __html: meal.instructions }}
+        ></p>
+      </main>
+    </>
+  );
 }
